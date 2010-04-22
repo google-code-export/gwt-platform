af115584dd5ee56c8b2f2f85b4a5d2038c980393
goudreau christian <goudreau.christian@gmail.com>
2010-04-22 17:21:31 +0000

    Complete - Task 4: Implement action-specific dispatch paths http://code.google.com/p/apapul/issues/detail?id=4
    Fixes issue 4.

--- src/com/philbeaudoin/gwtp/dispatch/client/secure/SecureDispatchAsync.java
+++ src/com/philbeaudoin/gwtp/dispatch/client/secure/SecureDispatchAsync.java
@@ -18,6 +18,7 @@ package com.philbeaudoin.gwtp.dispatch.client.secure;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.philbeaudoin.gwtp.dispatch.client.AbstractDispatchAsync;
 import com.philbeaudoin.gwtp.dispatch.client.DispatchAsync;
 import com.philbeaudoin.gwtp.dispatch.client.ExceptionHandler;
@@ -27,46 +28,53 @@ import com.philbeaudoin.gwtp.dispatch.shared.Result;
 
 /**
  * This class is the default implementation of {@link DispatchAsync}, which is
- * essentially the client-side access to the 
+ * essentially the client-side access to the
  * {@link com.philbeaudoin.gwtp.dispatch.server.Dispatch} class on the
  * server-side.
- *
+ * 
  * @author David Peterson
+ * @author Christian Goudreau
  */
 public class SecureDispatchAsync extends AbstractDispatchAsync {
-
-    private static final SecureDispatchServiceAsync realService = GWT.create( SecureDispatchService.class );
+    private static final SecureDispatchServiceAsync realService = GWT.create(SecureDispatchService.class);
+    private static final String baseUrl = ((ServiceDefTarget) realService).getServiceEntryPoint() + "/";
 
     private final SecureSessionAccessor secureSessionAccessor;
 
-    public SecureDispatchAsync( ExceptionHandler exceptionHandler, SecureSessionAccessor secureSessionAccessor ) {
-        super( exceptionHandler );
+    public SecureDispatchAsync(ExceptionHandler exceptionHandler, SecureSessionAccessor secureSessionAccessor) {
+        super(exceptionHandler);
         this.secureSessionAccessor = secureSessionAccessor;
     }
 
-    public <A extends Action<R>, R extends Result> void execute( final A action, final AsyncCallback<R> callback ) {
+    public <A extends Action<R>, R extends Result> void execute(final A action, final AsyncCallback<R> callback) {
+        String className = action.getClass().getName();
+        int namePos = className.lastIndexOf(".") + 1;
+        className = className.substring(namePos);
+
+        ((ServiceDefTarget) realService).setServiceEntryPoint(baseUrl + className);
 
         String sessionId = secureSessionAccessor.getSessionId();
 
-        realService.execute( sessionId, action, new AsyncCallback<Result>() {
-            public void onFailure( Throwable caught ) {
-                SecureDispatchAsync.this.onFailure( action, caught, callback );
+        realService.execute(sessionId, action, new AsyncCallback<Result>() {
+            public void onFailure(Throwable caught) {
+                SecureDispatchAsync.this.onFailure(action, caught, callback);
             }
 
-            @SuppressWarnings({"unchecked"})
-            public void onSuccess( Result result ) {
-                // Note: This cast is a dodgy hack to get around a GWT 1.6 async compiler issue
-                SecureDispatchAsync.this.onSuccess( action, (R) result, callback );
+            @SuppressWarnings( { "unchecked" })
+            public void onSuccess(Result result) {
+                // Note: This cast is a dodgy hack to get around a GWT 1.6 async
+                // compiler issue
+                SecureDispatchAsync.this.onSuccess(action, (R) result, callback);
             }
-        } );
+        });
     }
 
-    protected <A extends Action<R>, R extends Result> void onFailure( A action, Throwable caught, final AsyncCallback<R> callback ) {
-        if ( caught instanceof InvalidSessionException ) {
+    protected <A extends Action<R>, R extends Result> void onFailure(A action, Throwable caught, final AsyncCallback<R> callback) {
+        if (caught instanceof InvalidSessionException) {
             secureSessionAccessor.clearSessionId();
         }
 
-        super.onFailure( action, caught, callback );
+        super.onFailure(action, caught, callback);
 
     }
 }
