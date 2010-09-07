8dcf36ea31643585f37d3d7d8125a7785b4b99a0
Christian Goudreau <Christian Goudreau@localhost>
2010-09-07 10:23:07 -0400

    Renamed Controls to UiHandlers and added a new contructors for view's helper to be able to inject a provider and set the presenter automatically.

---
+++ test/com/gwtplatform/mvp/client/HandlerContainerImplTest.java
@@ -0,0 +1,178 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import com.gwtplatform.testing.GuiceMockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests for {@link HandlerContainerImpl}.
+ * 
+ * @author Philippe Beaudoin
+ */
+@RunWith(GuiceMockitoJUnitRunner.class)
+public class HandlerContainerImplTest {
+
+  // A subclass of HandlerContainerImpl that does not use autobinding
+  // and counts its inherited method invocations
+  static class NonAutoboundHandlerContainer extends HandlerContainerImpl {
+    public int onBindMethodCalled;
+    public int onUnbindMethodCalled;
+
+    @Inject
+    NonAutoboundHandlerContainer() {
+      super(false);
+    }
+
+    @Override
+    protected void onBind() {
+      super.onBind();
+      onBindMethodCalled++;
+    }
+
+    @Override
+    protected void onUnbind() {
+      super.onUnbind();
+      onUnbindMethodCalled++;
+    }
+  }
+
+  // Providers to use Guice injection
+  @Inject
+  Provider<HandlerContainerImpl> defaultHandlerContainerProvider;
+  @Inject
+  Provider<NonAutoboundHandlerContainer> nonAutoboundHandlerContainerProvider;
+
+  @Test
+  public void callingBindShouldInvokeOnBind() {
+    // Setup
+    NonAutoboundHandlerContainer handlerContainer = nonAutoboundHandlerContainerProvider.get();
+
+    // Given
+    handlerContainer.bind();
+
+    // When, Then
+    assertEquals(1, handlerContainer.onBindMethodCalled);
+  }
+
+  @Test
+  public void callingUnbindShouldInvokeOnUnbind() {
+    // Setup
+    NonAutoboundHandlerContainer handlerContainer = nonAutoboundHandlerContainerProvider.get();
+
+    // Given
+    handlerContainer.bind();
+    handlerContainer.unbind();
+
+    // When, Then
+    assertEquals(1, handlerContainer.onUnbindMethodCalled);
+  }
+
+  @Test
+  public void callingUnbindWhenUnboundShouldNotInvokeOnUnbind() {
+    // Setup
+    NonAutoboundHandlerContainer handlerContainer = nonAutoboundHandlerContainerProvider.get();
+
+    // Given
+    handlerContainer.bind();
+    handlerContainer.unbind();
+    handlerContainer.unbind();
+    handlerContainer.bind();
+    handlerContainer.unbind();
+    handlerContainer.unbind();
+
+    // When, Then
+    assertEquals(2, handlerContainer.onUnbindMethodCalled);
+  }
+
+  @Test
+  public void shouldBindDefaultHandlerContainerOnInjection() {
+    // Set-up
+    HandlerContainer handlerContainer = defaultHandlerContainerProvider.get();
+
+    // Given
+    // HandlerContainerImpl is injected
+
+    // When, Then
+    // the bind method should be injected at creation time
+    assertTrue(handlerContainer.isBound());
+  }
+
+  @Test
+  public void shouldNotBindNonAutoboundHandlerContainerOnInjection() {
+    // Set-up
+    HandlerContainer handlerContainer = nonAutoboundHandlerContainerProvider.get();
+
+    // Given
+    // A non-autobound subclass is injected
+
+    // When, Then
+    // the bind method should NOT be invoked at creation time
+    assertFalse(handlerContainer.isBound());
+  }
+
+  @Test
+  public void unbindingMultipleTimesRemoveHandlersOnlyOnce() {
+    // Setup
+    HandlerContainerImpl handlerContainer = defaultHandlerContainerProvider.get();
+    HandlerRegistration mockHandlerRegistration = mock(HandlerRegistration.class);
+
+    // Given
+    handlerContainer.registerHandler(mockHandlerRegistration);
+
+    // When
+    handlerContainer.unbind();
+    handlerContainer.bind();
+    handlerContainer.unbind();
+    handlerContainer.unbind();
+
+    // Then
+    verify(mockHandlerRegistration).removeHandler();
+  }
+
+  @Test
+  public void unbindingRemoveHandlers() {
+    // Setup
+    HandlerContainerImpl handlerContainer = defaultHandlerContainerProvider.get();
+    HandlerRegistration mockHandlerRegistration1 = mock(HandlerRegistration.class);
+    HandlerRegistration mockHandlerRegistration2 = mock(HandlerRegistration.class);
+
+    // Given
+    handlerContainer.registerHandler(mockHandlerRegistration1);
+    handlerContainer.registerHandler(mockHandlerRegistration2);
+
+    // When
+    handlerContainer.unbind();
+
+    // Then
+    verify(mockHandlerRegistration1).removeHandler();
+    verify(mockHandlerRegistration2).removeHandler();
+  }
+
+}
---
+++ test/com/gwtplatform/mvp/client/PresenterTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.testing.GuiceMockitoJUnitRunner;
+import com.gwtplatform.testing.TestModule;
+import com.gwtplatform.testing.TestScope;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests for {@link Presenter}.
+ * 
+ * @author Philippe Beaudoin
+ */
+@RunWith(GuiceMockitoJUnitRunner.class)
+public class PresenterTest {
+  /**
+   * Guice environment.
+   */
+  public static class Env extends TestModule {
+    @Override
+    protected void configure() {
+      bindMock(EventBus.class).in(TestScope.SINGLETON);
+      bindMock(ViewA.class).in(TestScope.SINGLETON);
+      bindMock(ProxyA.class).in(TestScope.SINGLETON);
+      bind(PresenterA.class).in(TestScope.SINGLETON);
+    }
+  }
+
+  static class PresenterA extends PresenterSpy<ViewA, ProxyA> {
+    @Inject
+    public PresenterA(EventBus eventBus, ViewA view, ProxyA proxy) {
+      super(eventBus, view, proxy);
+    }
+  }
+  // Simple subclasses of PresenterWidgetImpl
+  abstract static class PresenterSpy<V extends View, P extends Proxy<?>>
+      extends Presenter<V, P> {
+    public int revealInParentCalled;
+
+    PresenterSpy(EventBus eventBus, V view, P proxy) {
+      super(eventBus, view, proxy);
+    }
+
+    @Override
+    public void revealInParent() {
+      super.onReveal();
+      revealInParentCalled++;
+    }
+  }
+
+  interface ProxyA extends Proxy<PresenterA> {
+  }
+
+  interface ViewA extends View {
+  }
+
+  // Providers to use Guice injection
+  @Inject
+  Provider<PresenterA> presenterAProvider;
+  @Inject
+  Provider<ProxyA> proxyAProvider;
+  @Inject
+  Provider<ViewA> viewAProvider;
+
+  @Test
+  public void forceRevealWhenPresenterIsNotVisible() {
+    // Set-up
+    PresenterA presenter = presenterAProvider.get();
+
+    // Given
+    assertFalse(presenter.isVisible());
+
+    // When
+    presenter.forceReveal();
+
+    // Then
+    assertEquals(1, presenter.revealInParentCalled);
+  }
+
+  @Test
+  public void forceRevealWhenPresenterIsVisible() {
+    // Set-up
+    PresenterA presenter = presenterAProvider.get();
+
+    // Given
+    presenter.reveal();
+    assertTrue(presenter.isVisible());
+
+    // When
+    presenter.forceReveal();
+
+    // Then
+    assertEquals(0, presenter.revealInParentCalled);
+  }
+}
