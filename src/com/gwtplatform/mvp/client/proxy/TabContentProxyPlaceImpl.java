9de388436cc6206a1109cd17bb6e0560605a63d2
Brendan Doherty <brendanpdoherty@gmail.com>
2010-09-18 08:43:10 +1200

    Deprecate existing bindMockHandler for bindMockActionHandler

---
+++ src/com/gwtplatform/mvp/client/proxy/TabContentProxyPlaceImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.proxy;
+
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.Tab;
+
+/**
+ * A useful mixing class to define a {@link TabContentProxy} that is also a
+ * {@link Place}.
+ * 
+ * @param <T> The Presenter's type.
+ * 
+ * @author Philippe Beaudoin
+ */
+public class TabContentProxyPlaceImpl<T extends Presenter<?, ?>> extends
+    ProxyPlaceAbstract<T, TabContentProxy<T>> implements
+    TabContentProxyPlace<T> {
+
+  public TabContentProxyPlaceImpl() {
+  }
+
+  @Override
+  public String getHistoryToken() {
+    return getNameToken();
+  }
+
+  @Override
+  public String getLabel() {
+    return proxy.getLabel();
+  }
+
+  @Override
+  public float getPriority() {
+    return proxy.getPriority();
+  }
+
+  @Override
+  public Tab getTab() {
+    return proxy.getTab();
+  }
+
+}
