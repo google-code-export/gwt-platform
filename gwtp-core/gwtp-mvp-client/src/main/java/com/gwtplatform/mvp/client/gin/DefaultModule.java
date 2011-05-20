7c24ac60695f1834af098d8d424aba4cc9bbc6bd
Yannis Gonianakis <jgonian@gmail.com>
2011-05-20 19:48:37 +0200

    Declared gwt-servlet in the dependencyManagement section and in the gwt-maven-plugin (overrode the plugin 's dependency).

---
+++ gwtp-core/gwtp-mvp-client/src/main/java/com/gwtplatform/mvp/client/gin/DefaultModule.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.gin;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.inject.Singleton;
+import com.gwtplatform.mvp.client.RootPresenter;
+import com.gwtplatform.mvp.client.googleanalytics.GoogleAnalytics;
+import com.gwtplatform.mvp.client.googleanalytics.GoogleAnalyticsImpl;
+import com.gwtplatform.mvp.client.proxy.ParameterTokenFormatter;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.TokenFormatter;
+
+/**
+ * Module with default GWTP bindings. You can
+ * {@code install(new DefaultModule(MyPlaceManager.class))}
+ * instead of manually binding the different classes to their default implementation.
+ *
+ * @author Christian Goudreau
+ */
+public class DefaultModule extends AbstractGinModule {
+
+  private final Class<? extends PlaceManager> placeManagerClass;
+
+  public DefaultModule(Class<? extends PlaceManager> placeManagerClass) {
+    this.placeManagerClass = placeManagerClass;
+  }
+
+  @Override
+  protected void configure() {
+    bind(EventBus.class).to(SimpleEventBus.class).in(Singleton.class);
+    bind(TokenFormatter.class).to(ParameterTokenFormatter.class).in(Singleton.class);
+    bind(RootPresenter.class).asEagerSingleton();
+    bind(GoogleAnalytics.class).to(GoogleAnalyticsImpl.class).in(Singleton.class);
+    bind(PlaceManager.class).to(placeManagerClass).in(Singleton.class);
+  }
+}
---
+++ gwtp-core/gwtp-mvp-client/src/test/java/com/gwtplatform/mvp/client/gwt/mvp/AdminViewTestUtilGwt.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2010 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.mvp.client.gwt.mvp;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.gwtplatform.mvp.client.ViewImpl;
+
+/**
+ * A test presenter meant to be run in a GWTTestCase.
+ *
+ * @author Philippe Beaudoin
+ */
+public class AdminViewTestUtilGwt extends ViewImpl implements AdminPresenterTestUtilGwt.MyView {
+
+  public final FlowPanel widget;
+
+  public AdminViewTestUtilGwt() {
+    widget = new FlowPanel();
+  }
+
+  @Override
+  public Widget asWidget() {
+    return widget;
+  }
+}
